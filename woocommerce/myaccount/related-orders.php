16739b1dc0a8bc738d5718711fc2765c7d397865
Tim Loden <tim.m.loden@gmail.com>
2021-01-17 10:03:19 -0800

    thank you page

--- woocommerce/myaccount/related-orders.php
+++ woocommerce/myaccount/related-orders.php
@@ -12,23 +12,31 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 <header>
-	<h2><?php esc_html_e( 'Related orders', 'woocommerce-subscriptions' ); ?></h2>
+    <h2><?php esc_html_e( 'Related orders', 'woocommerce-subscriptions' ); ?></h2>
 </header>
 
-<table class="shop_table shop_table_responsive my_account_orders woocommerce-orders-table woocommerce-MyAccount-orders woocommerce-orders-table--orders">
+<table
+    class="shop_table shop_table_responsive my_account_orders woocommerce-orders-table woocommerce-MyAccount-orders woocommerce-orders-table--orders table">
 
-	<thead>
-		<tr>
-			<th class="order-number woocommerce-orders-table__header woocommerce-orders-table__header-order-number"><span class="nobr"><?php esc_html_e( 'Order', 'woocommerce-subscriptions' ); ?></span></th>
-			<th class="order-date woocommerce-orders-table__header woocommerce-orders-table__header-order-date woocommerce-orders-table__header-order-date"><span class="nobr"><?php esc_html_e( 'Date', 'woocommerce-subscriptions' ); ?></span></th>
-			<th class="order-status woocommerce-orders-table__header woocommerce-orders-table__header-order-status"><span class="nobr"><?php esc_html_e( 'Status', 'woocommerce-subscriptions' ); ?></span></th>
-			<th class="order-total woocommerce-orders-table__header woocommerce-orders-table__header-order-total"><span class="nobr"><?php echo esc_html_x( 'Total', 'table heading', 'woocommerce-subscriptions' ); ?></span></th>
-			<th class="order-actions woocommerce-orders-table__header woocommerce-orders-table__header-order-actions">&nbsp;</th>
-		</tr>
-	</thead>
+    <thead>
+        <tr>
+            <th class="order-number woocommerce-orders-table__header woocommerce-orders-table__header-order-number">
+                <span class="nobr"><?php esc_html_e( 'Order', 'woocommerce-subscriptions' ); ?></span></th>
+            <th
+                class="order-date woocommerce-orders-table__header woocommerce-orders-table__header-order-date woocommerce-orders-table__header-order-date">
+                <span class="nobr"><?php esc_html_e( 'Date', 'woocommerce-subscriptions' ); ?></span></th>
+            <th class="order-status woocommerce-orders-table__header woocommerce-orders-table__header-order-status">
+                <span class="nobr"><?php esc_html_e( 'Status', 'woocommerce-subscriptions' ); ?></span></th>
+            <th class="order-total woocommerce-orders-table__header woocommerce-orders-table__header-order-total"><span
+                    class="nobr"><?php echo esc_html_x( 'Total', 'table heading', 'woocommerce-subscriptions' ); ?></span>
+            </th>
+            <th class="order-actions woocommerce-orders-table__header woocommerce-orders-table__header-order-actions">
+                &nbsp;</th>
+        </tr>
+    </thead>
 
-	<tbody>
-		<?php foreach ( $subscription_orders as $subscription_order ) :
+    <tbody>
+        <?php foreach ( $subscription_orders as $subscription_order ) :
 			$order = wc_get_order( $subscription_order );
 
 			if ( ! $order ) {
@@ -38,26 +46,32 @@ if ( ! defined( 'ABSPATH' ) ) {
 			$item_count = $order->get_item_count();
 			$order_date = $order->get_date_created();
 
-			?><tr class="order woocommerce-orders-table__row woocommerce-orders-table__row--status-<?php echo esc_attr( $order->get_status() ); ?>">
-				<td class="order-number woocommerce-orders-table__cell woocommerce-orders-table__cell-order-number" data-title="<?php esc_attr_e( 'Order Number', 'woocommerce-subscriptions' ); ?>">
-					<a href="<?php echo esc_url( $order->get_view_order_url() ); ?>">
-						<?php echo sprintf( esc_html_x( '#%s', 'hash before order number', 'woocommerce-subscriptions' ), esc_html( $order->get_order_number() ) ); ?>
-					</a>
-				</td>
-				<td class="order-date woocommerce-orders-table__cell woocommerce-orders-table__cell-order-date" data-title="<?php esc_attr_e( 'Date', 'woocommerce-subscriptions' ); ?>">
-					<time datetime="<?php echo esc_attr( $order_date->date( 'Y-m-d' ) ); ?>" title="<?php echo esc_attr( $order_date->getTimestamp() ); ?>"><?php echo wp_kses_post( $order_date->date_i18n( wc_date_format() ) ); ?></time>
-				</td>
-				<td class="order-status woocommerce-orders-table__cell woocommerce-orders-table__cell-order-status" data-title="<?php esc_attr_e( 'Status', 'woocommerce-subscriptions' ); ?>" style="white-space:nowrap;">
-					<?php echo esc_html( wc_get_order_status_name( $order->get_status() ) ); ?>
-				</td>
-				<td class="order-total woocommerce-orders-table__cell woocommerce-orders-table__cell-order-total" data-title="<?php echo esc_attr_x( 'Total', 'Used in data attribute. Escaped', 'woocommerce-subscriptions' ); ?>">
-					<?php
+			?><tr
+            class="order woocommerce-orders-table__row woocommerce-orders-table__row--status-<?php echo esc_attr( $order->get_status() ); ?>">
+            <td class="order-number woocommerce-orders-table__cell woocommerce-orders-table__cell-order-number"
+                data-title="<?php esc_attr_e( 'Order Number', 'woocommerce-subscriptions' ); ?>">
+                <a href="<?php echo esc_url( $order->get_view_order_url() ); ?>">
+                    <?php echo sprintf( esc_html_x( '#%s', 'hash before order number', 'woocommerce-subscriptions' ), esc_html( $order->get_order_number() ) ); ?>
+                </a>
+            </td>
+            <td class="order-date woocommerce-orders-table__cell woocommerce-orders-table__cell-order-date"
+                data-title="<?php esc_attr_e( 'Date', 'woocommerce-subscriptions' ); ?>">
+                <time datetime="<?php echo esc_attr( $order_date->date( 'Y-m-d' ) ); ?>"
+                    title="<?php echo esc_attr( $order_date->getTimestamp() ); ?>"><?php echo wp_kses_post( $order_date->date_i18n( wc_date_format() ) ); ?></time>
+            </td>
+            <td class="order-status woocommerce-orders-table__cell woocommerce-orders-table__cell-order-status"
+                data-title="<?php esc_attr_e( 'Status', 'woocommerce-subscriptions' ); ?>" style="white-space:nowrap;">
+                <?php echo esc_html( wc_get_order_status_name( $order->get_status() ) ); ?>
+            </td>
+            <td class="order-total woocommerce-orders-table__cell woocommerce-orders-table__cell-order-total"
+                data-title="<?php echo esc_attr_x( 'Total', 'Used in data attribute. Escaped', 'woocommerce-subscriptions' ); ?>">
+                <?php
 					// translators: $1: formatted order total for the order, $2: number of items bought
 					echo wp_kses_post( sprintf( _n( '%1$s for %2$d item', '%1$s for %2$d items', $item_count, 'woocommerce-subscriptions' ), $order->get_formatted_order_total(), $item_count ) );
 					?>
-				</td>
-				<td class="order-actions woocommerce-orders-table__cell woocommerce-orders-table__cell-order-actions">
-					<?php $actions = array();
+            </td>
+            <td class="order-actions woocommerce-orders-table__cell woocommerce-orders-table__cell-order-actions">
+                <?php $actions = array();
 
 					if ( $order->needs_payment() && wcs_get_objects_property( $order, 'id' ) == $subscription->get_last_order( 'ids', 'any' ) ) {
 						$actions['pay'] = array(
@@ -92,10 +106,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 						}
 					}
 					?>
-				</td>
-			</tr>
-		<?php endforeach; ?>
-	</tbody>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
 </table>
 
 <?php do_action( 'woocommerce_subscription_details_after_subscription_related_orders_table', $subscription ); ?>
