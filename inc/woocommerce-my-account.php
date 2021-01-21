682e0463ee8b0b917d3945c5bd29b3c393162f4c
Tim Loden <tim.m.loden@gmail.com>
2021-01-20 20:15:44 -0800

    removed downloads

---
+++ inc/woocommerce-my-account.php
@@ -0,0 +1,8 @@
+<?php
+
+add_filter( 'woocommerce_account_menu_items', 'custom_remove_downloads_my_account', 999 );
+ 
+function custom_remove_downloads_my_account( $items ) {
+unset($items['downloads']);
+return $items;
+}
