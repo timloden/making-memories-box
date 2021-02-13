3f147c4e69902b65dcfa8be04f2568b7ee727b9e
Tim Loden <tim.m.loden@gmail.com>
2021-02-12 23:12:38 -0800

    re arranged the how it works section

--- page-home.php
+++ page-home.php
@@ -49,74 +49,65 @@ get_header();
 <section id="how-it-works" class="home-how-it-works py-5"
     style="background: url(<?php echo get_template_directory_uri(); ?>/assets/images/blue-circle-right.png) center top / cover;">
     <div class="container">
-        <div class="row pb-5">
+
+        <div class="row pb-3">
             <div class="col-12">
-                <h2 class="text-center"><?php echo esc_attr(the_field('how_it_works_title')); ?></h2>
-                <p class="text-center text-black-50"><?php echo esc_attr(the_field('how_it_works_subtitle')); ?></p>
+                <h2 class="text-center pb-3"><?php echo esc_attr(the_field('how_it_works_title')); ?></h2>
             </div>
         </div>
-        <div class="row align-items-center">
-            <div class="col-12 col-lg-6 order-1 order-lg-0">
-                <div class="d-flex">
-                    <div class="col-6 px-0">
-                        <div class="d-flex flex-column">
-                            <div class="col pb-4">
-                                <?php 
-                            $image_1 = get_field('how_it_works_image_1');
-                            if( !empty( $image_1 ) ): ?>
-                                <img class="img-fluid shadow" src="<?php echo esc_url($image_1['url']); ?>"
-                                    data-aos="fade-down" data-aos-delay="100" data-aos-once="true"
-                                    alt="<?php echo esc_attr($image_1['alt']); ?>">
-                                <?php endif; ?>
-                            </div>
-                            <div class="col">
-                                <?php 
-                            $image_3 = get_field('how_it_works_image_3');
-                            if( !empty( $image_3 ) ): ?>
-                                <img class="img-fluid shadow" src="<?php echo esc_url($image_3['url']); ?>"
-                                    data-aos="fade-down" data-aos-delay="300" data-aos-once="true"
-                                    alt="<?php echo esc_attr($image_3['alt']); ?>">
-                                <?php endif; ?>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-6 px-0">
-                        <div class="d-flex flex-column justify-content-center h-100">
-                            <div class="col pt-5 pb-4">
-                                <?php 
-                            $image_2 = get_field('how_it_works_image_2');
-                            if( !empty( $image_2 ) ): ?>
-                                <img class="img-fluid shadow" src="<?php echo esc_url($image_2['url']); ?>"
-                                    data-aos="fade-down" data-aos-delay="200" data-aos-once="true"
-                                    alt="<?php echo esc_url($image_2['alt']); ?>">
-                                <?php endif; ?>
-                            </div>
-                            <div class="col">
-                                <?php 
-                            $image_4 = get_field('how_it_works_image_4');
-                            if( !empty( $image_4 ) ): ?>
-                                <img class="img-fluid shadow" src="<?php echo esc_url($image_4['url']); ?>"
-                                    data-aos="fade-down" data-aos-delay="400" data-aos-once="true"
-                                    alt="<?php echo esc_url($image_4['alt']); ?>">
-                                <?php endif; ?>
-                            </div>
-                        </div>
+
+        <div class="row mb-5">
+            <?php if( have_rows('how_it_works_steps') ): ?>
+            <?php while( have_rows('how_it_works_steps') ): the_row(); ?>
+            <div class="col-12 col-lg-4">
+                <div class="card shadow p-3 border-0 text-center">
+                    <h4 class="pb-2"><?php echo esc_attr(the_sub_field('how_it_works_step_title')); ?></h4>
+                    <div class="text-black-50">
+                        <p class="mb-0"><?php echo esc_attr(the_sub_field('how_is_works_step_description')); ?></p>
                     </div>
                 </div>
             </div>
-            <div class="col-12 col-lg-6 h-100 order-0 order-lg-1">
-                <div class="pl-lg-3 pb-5 pb-lg-0">
-                    <?php if( have_rows('how_it_works_steps') ): ?>
-                    <?php while( have_rows('how_it_works_steps') ): the_row(); ?>
-                    <h3><?php echo esc_attr(the_sub_field('how_it_works_step_title')); ?></h3>
-                    <div class="text-black-50 mb-4">
-                        <p><?php echo esc_attr(the_sub_field('how_is_works_step_description')); ?></p>
-                    </div>
-                    <?php endwhile; ?>
-                    <?php endif; ?>
-                    <a href="<?php echo esc_url(the_field('how_it_works_button_link')); ?>"
-                        class="btn btn-primary btn-rounded btn-lg btn-block d-block d-lg-inline-block"><?php echo esc_attr(the_field('how_it_works_button_text')); ?></a>
-                </div>
+            <?php endwhile; ?>
+            <?php endif; ?>
+        </div>
+        <div class="row mb-5">
+            <div class="col-6 col-lg-3">
+                <?php 
+                            $image_1 = get_field('how_it_works_image_1');
+                            if( !empty( $image_1 ) ): ?>
+                <img class="img-fluid shadow" src="<?php echo esc_url($image_1['url']); ?>" data-aos="fade-down"
+                    data-aos-delay="100" data-aos-once="true" alt="<?php echo esc_attr($image_1['alt']); ?>">
+                <?php endif; ?>
+            </div>
+            <div class="col-6 col-lg-3">
+                <?php 
+                            $image_2 = get_field('how_it_works_image_2');
+                            if( !empty( $image_2 ) ): ?>
+                <img class="img-fluid shadow" src="<?php echo esc_url($image_2['url']); ?>" data-aos="fade-down"
+                    data-aos-delay="200" data-aos-once="true" alt="<?php echo esc_attr($image_2['alt']); ?>">
+                <?php endif; ?>
+            </div>
+            <div class="col-6 col-lg-3">
+                <?php 
+                            $image_3 = get_field('how_it_works_image_3');
+                            if( !empty( $image_3 ) ): ?>
+                <img class="img-fluid shadow" src="<?php echo esc_url($image_3['url']); ?>" data-aos="fade-down"
+                    data-aos-delay="300" data-aos-once="true" alt="<?php echo esc_attr($image_3['alt']); ?>">
+                <?php endif; ?>
+            </div>
+            <div class="col-6 col-lg-3">
+                <?php 
+                            $image_4 = get_field('how_it_works_image_4');
+                            if( !empty( $image_4 ) ): ?>
+                <img class="img-fluid shadow" src="<?php echo esc_url($image_4['url']); ?>" data-aos="fade-down"
+                    data-aos-delay="400" data-aos-once="true" alt="<?php echo esc_attr($image_4['alt']); ?>">
+                <?php endif; ?>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 text-center">
+                <a href="<?php echo esc_url(the_field('how_it_works_button_link')); ?>"
+                    class="btn btn-primary btn-rounded btn-lg d-block d-lg-inline-block"><?php echo esc_attr(the_field('how_it_works_button_text')); ?></a>
             </div>
         </div>
     </div>
@@ -131,18 +122,6 @@ get_header();
                         class="text-primary"><?php echo esc_attr(the_field('bottom_special_offer_text')); ?></a></p>
             </div>
         </div>
-        <?php 
-        $bottom_box_image = get_field('bottom_box_image');
-        if( !empty( $bottom_box_image ) ): ?>
-
-        <div class="row">
-            <div class="col-12 text-center">
-                <img src="<?php echo esc_url($bottom_box_image['url']); ?>" class="img-fluid"
-                    alt="<?php echo esc_url($bottom_box_image['alt']); ?>">
-            </div>
-        </div>
-
-        <?php endif; ?>
     </div>
 </section>
 
